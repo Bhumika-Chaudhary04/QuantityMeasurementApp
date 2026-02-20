4ce057a158602adaef7f6394ec04e0b82f1d7eda
Bhumika Chaudhary <bhumi333333@gmail.com>
2026-02-20 23:39:41 +0530

    [bhumika chaudhary] Refactor .Added UC3: Implement generic Quantity Length with unit enum to eliminate duplication

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,121 +1,52 @@
 package com.apps.quantitymeasurement;
 
-import java.util.Scanner;
-import java.util.InputMismatchException;
-
 public class QuantityMeasurementApp {
-	// UC1
-	// inner Class
-	public static class Feet {
-		private final double value;
 
-		// constructor
-		public Feet(double value) {
-			this.value = value;
+	//generic method to demonstrate Length equality check
+	public static boolean demonstrateLengthEquality(Length length1, Length length2) {
+		if (length1 == null || length2 == null) {
+			return false;
 		}
 
-		public double getValue() {
-			return value;
-		}
-
-		// overriding equals() method
-		@Override
-		public boolean equals(Object obj) {
-
-			if (this == obj)
-				return true;
-
-			if (obj == null)
-				return false;
-
-			if (getClass() != obj.getClass())
-				return false;
-
-			Feet other = (Feet) obj;
-
-			return Double.compare(this.value, other.value) == 0;
-		}
-
-		@Override
-		public int hashCode() {
-			return Double.hashCode(value);
-		}
-	}
-	//UC2
-	public static class Inches {
-		private final double value;
-
-		public Inches(double value) {
-			this.value = value;
-		}
-
-		public double getValue() {
-			return value;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-
-			if (this == obj)
-				return true;
-
-			if (obj == null)
-				return false;
-
-			if (getClass() != obj.getClass())
-				return false;
-
-			Inches other = (Inches) obj;
-
-			return Double.compare(this.value, other.value) == 0;
-		}
-
-		@Override
-		public int hashCode() {
-			return Double.hashCode(value);
-		}
+		boolean result = length1.equals(length2);
+		System.out.println("Comparing: " + length1 + " and " + length2);
+		System.out.println("Are equal? " + result);
+		return result;
 	}
 
-	// feet equality check
-	public static boolean compareFeet(double value1, double value2) {
-		Feet feet1 = new Feet(value1);
-		Feet feet2 = new Feet(value2);
-		return feet1.equals(feet2);
+	//demonstrate Feet equality
+	public static void demonstrateFeetEquality() {
+		Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+		Length length2 = new Length(1.0, Length.LengthUnit.FEET);
+
+		System.out.println("Feet to Feet Comparison:");
+		demonstrateLengthEquality(length1, length2);
+		System.out.println();
 	}
 
-	// inches equality check
-	public static boolean compareInches(double value1, double value2) {
-		Inches inch1 = new Inches(value1);
-		Inches inch2 = new Inches(value2);
-		return inch1.equals(inch2);
+	//demonstrate Inches equality
+	public static void demonstrateInchesEquality() {
+		Length length1 = new Length(1.0, Length.LengthUnit.INCHES);
+		Length length2 = new Length(1.0, Length.LengthUnit.INCHES);
+
+		System.out.println("Inches to Inches Comparison:");
+		demonstrateLengthEquality(length1, length2);
+		System.out.println();
 	}
 
-	// main method with user input validation
+	//demonstrate Feet and Inches comparison
+	public static void demonstrateFeetInchesComparison() {
+		Length length1 = new Length(1.0, Length.LengthUnit.FEET);
+		Length length2 = new Length(12.0, Length.LengthUnit.INCHES);
+
+		System.out.println("Feet to Inches Comparison:");
+		demonstrateLengthEquality(length1, length2);
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-	    try {
-	        System.out.println("Enter first value in Feet:");
-	        double feet1 = Double.parseDouble(sc.nextLine());
-
-	        System.out.println("Enter second value in Feet:");
-	        double feet2 = Double.parseDouble(sc.nextLine());
-
-	        boolean feetResult = compareFeet(feet1, feet2);
-	        System.out.println("Feet Comparison Result: " + feetResult);
-
-	        System.out.println("\nEnter first value in Inches:");
-	        double inch1 = Double.parseDouble(sc.nextLine());
-
-	        System.out.println("Enter second value in Inches:");
-	        double inch2 = Double.parseDouble(sc.nextLine());
-
-	        boolean inchResult = compareInches(inch1, inch2);
-	        System.out.println("Inches Comparison Result: " + inchResult);
-
-	    } catch (NumberFormatException e) {
-	        System.out.println("Invalid input! Please enter numeric values only.");
-	    }
-	    sc.close();
+		demonstrateFeetEquality();
+		demonstrateInchesEquality();
+		demonstrateFeetInchesComparison();
 	}
 }
