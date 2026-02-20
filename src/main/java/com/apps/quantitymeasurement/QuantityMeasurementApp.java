373cfee247a2112a5aa0afe52c5fb287c892deb5
Bhumika Chaudhary <bhumi333333@gmail.com>
2026-02-20 23:28:39 +0530

    [bhumika chaudhary] Add .Added UC2 : Implement Inch measurement equality

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class QuantityMeasurementApp {
-	//UC1
+	// UC1
 	// inner Class
 	public static class Feet {
 		private final double value;
@@ -35,35 +35,87 @@ public class QuantityMeasurementApp {
 
 			return Double.compare(this.value, other.value) == 0;
 		}
+
 		@Override
 		public int hashCode() {
-		    return Double.hashCode(value);
+			return Double.hashCode(value);
 		}
+	}
+	//UC2
+	public static class Inches {
+		private final double value;
+
+		public Inches(double value) {
+			this.value = value;
+		}
+
+		public double getValue() {
+			return value;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+
+			if (this == obj)
+				return true;
+
+			if (obj == null)
+				return false;
+
+			if (getClass() != obj.getClass())
+				return false;
+
+			Inches other = (Inches) obj;
+
+			return Double.compare(this.value, other.value) == 0;
+		}
+
+		@Override
+		public int hashCode() {
+			return Double.hashCode(value);
+		}
+	}
+
+	// feet equality check
+	public static boolean compareFeet(double value1, double value2) {
+		Feet feet1 = new Feet(value1);
+		Feet feet2 = new Feet(value2);
+		return feet1.equals(feet2);
+	}
+
+	// inches equality check
+	public static boolean compareInches(double value1, double value2) {
+		Inches inch1 = new Inches(value1);
+		Inches inch2 = new Inches(value2);
+		return inch1.equals(inch2);
 	}
 
 	// main method with user input validation
 	public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+	    try {
+	        System.out.println("Enter first value in Feet:");
+	        double feet1 = Double.parseDouble(sc.nextLine());
 
-		try {
-			System.out.print("Enter first value in feet: ");
-			double value1 = scanner.nextDouble();
+	        System.out.println("Enter second value in Feet:");
+	        double feet2 = Double.parseDouble(sc.nextLine());
 
-			System.out.print("Enter second value in feet: ");
-			double value2 = scanner.nextDouble();
+	        boolean feetResult = compareFeet(feet1, feet2);
+	        System.out.println("Feet Comparison Result: " + feetResult);
 
-			Feet feet1 = new Feet(value1);
-			Feet feet2 = new Feet(value2);
+	        System.out.println("\nEnter first value in Inches:");
+	        double inch1 = Double.parseDouble(sc.nextLine());
 
-			System.out.println("Are they equal? " + feet1.equals(feet2));
+	        System.out.println("Enter second value in Inches:");
+	        double inch2 = Double.parseDouble(sc.nextLine());
 
-		} 
-		catch (InputMismatchException e) {
-			System.out.println("Invalid input! Please enter numeric values only.");
-		} 
-		finally {
-			scanner.close();
-		}
+	        boolean inchResult = compareInches(inch1, inch2);
+	        System.out.println("Inches Comparison Result: " + inchResult);
+
+	    } catch (NumberFormatException e) {
+	        System.out.println("Invalid input! Please enter numeric values only.");
+	    }
+	    sc.close();
 	}
 }
--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 	//UC1
@@ -44,4 +45,38 @@ public class QuantityMeasurementAppTest {
 
 		assertTrue(feet.equals(feet));
 	}
+    //UC2
+	@Test
+    public void testInchesEquality_SameValue() {
+        Inches i1 = new Inches(10.0);
+        Inches i2 = new Inches(10.0);
+        assertEquals(i1, i2);
+    }
+
+    @Test
+    public void testInchesEquality_DifferentValue() {
+        Inches i1 = new Inches(10.0);
+        Inches i2 = new Inches(20.0);
+        assertNotEquals(i1, i2);
+    }
+
+    @Test
+    public void testInchesEquality_NullComparison() {
+        Inches i1 = new Inches(10.0);
+        assertNotEquals(i1, null);
+    }
+
+    @Test
+    public void testInchesEquality_DifferentClass() {
+        Inches i1 = new Inches(10.0);
+        Feet feet = new Feet(10.0);
+        assertNotEquals(i1, feet);
+    }
+
+    @Test
+    public void testInchesEquality_SameReference() {
+        Inches i1 = new Inches(10.0);
+        Inches i2 = i1;
+        assertEquals(i1, i2);
+    }
 }
